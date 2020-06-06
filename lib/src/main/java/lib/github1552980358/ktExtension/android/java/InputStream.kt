52cd8aea7a7460548f8814b9889be527446cdabb
1552980358 <1552980358@qq.com>
2020-06-06 16:30:56 +0800

    InputStream.kt: Add reading to Bitmap and Drawable

---
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/java/InputStream.kt
@@ -0,0 +1,27 @@
+package lib.github1552980358.ktExtension.android.java
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import lib.github1552980358.ktExtension.jvm.javaKeyword.tryCatch
+import java.io.InputStream
+
+/**
+ * Read [Bitmap] from [InputStream]
+ **/
+fun InputStream.readBitmap(): Bitmap? {
+    var bitmap: Bitmap? = null
+    tryCatch { bitmap = BitmapFactory.decodeStream(this) }
+    close()
+    return bitmap
+}
+
+/**
+ * Read [Drawable] from [InputStream]
+ **/
+fun InputStream.readDrawable(srcName: String = "DRAWABLE"): Drawable? {
+    var drawable: Drawable? = null
+    tryCatch { drawable = Drawable.createFromStream(this, srcName) }
+    close()
+    return drawable
+}
