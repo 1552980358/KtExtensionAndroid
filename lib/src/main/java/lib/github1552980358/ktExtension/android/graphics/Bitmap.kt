44a3d095e13ba6aa19fb9e310727c608336fc1b2
1552980358 <1552980358@qq.com>
2020-06-06 15:36:13 +0800

    Bitmap.kt: Add Bitmap.cutCircle

--- lib/src/main/java/lib/github1552980358/ktExtension/android/graphics/Bitmap.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/graphics/Bitmap.kt
@@ -15,6 +15,7 @@ import lib.github1552980358.ktExtension.jvm.javaKeyword.tryCatch
 import lib.github1552980358.ktExtension.jvm.stream.osApply
 import java.io.ByteArrayOutputStream
 import java.io.File
+import kotlin.math.abs
 
 /** Output into file **/
 fun Bitmap.writePNGToFile(file: File, quality: Int = 100) {
@@ -74,14 +75,14 @@ fun Bitmap.cutSquare(): Bitmap? {
     }
     
     return when {
-        width == height -> {
-            Bitmap.createBitmap(this, 0, 0, width, width)
-        }
         width > height -> {
             Bitmap.createBitmap(this, (width - height) / 2, 0, width, width)
         }
-        else -> {
+        width < height -> {
             Bitmap.createBitmap(this, 0, (height - width) / 2, width, width)
+        }
+        else -> {
+            this
         }
     }
     
@@ -100,5 +101,60 @@ fun Bitmap.cutSquare(centerX: Int, centerY: Int, sideLength: Int): Bitmap? {
         return null
     }
     
-    return Bitmap.createBitmap(this, (centerX - sideLength / 2), (centerY - sideLength/ 2) , sideLength, sideLength)
+    return Bitmap.createBitmap(this, (centerX - sideLength) / 2, (centerY - sideLength) / 2, sideLength, sideLength)
+}
+
+/**
+ * Cut image into circular
+ **/
+fun Bitmap.cutCircle(): Bitmap? {
+    return (cutSquare() ?: return null).apply {
+        cutCircleInternal(this, Canvas(this), Paint().apply { isAntiAlias = true })
+    }
+}
+
+fun Bitmap.cutCircle(centerX: Int, centerY: Int, radius: Int): Bitmap? {
+    // At the edge
+    if (centerX == 0 || centerX == width || centerY == 0 || centerY == height) {
+        return null
+    }
+    
+    // Diameter larger than size of Bitmap
+    if (radius * 2 > width || radius * 2 > height) {
+        return null
+    }
+    
+    // Prevent radius exceeded
+    if ((centerX + radius) > height || (abs(centerX - radius) > centerX)) {
+        return null
+    }
+    if ((centerY + radius) > width || (abs(centerY - radius) > centerY)) {
+        return null
+    }
+    
+    // Cut into square
+    return cutSquare(centerX, centerY, radius * 2).apply {
+        this?:return null
+        cutCircleInternal(this, Canvas( this), Paint().apply { isAntiAlias = true })
+    }
+}
+
+/**
+ * For internal use
+ * @HIDE
+ **/
+private fun cutCircleInternal(bitmap: Bitmap, canvas: Canvas, paint: Paint) {
+    canvas.drawCircle(
+        bitmap.width / 2F,
+        bitmap.height / 2F,
+        bitmap.width.toFloat(),
+        paint
+    )
+    paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+    canvas.drawBitmap(
+        bitmap,
+        0F,
+        0F,
+        paint
+    )
 }
