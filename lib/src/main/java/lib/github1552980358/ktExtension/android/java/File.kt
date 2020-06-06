75509378995a3b278caa30bce04ff190930e880b
1552980358 <1552980358@qq.com>
2020-06-06 10:26:20 +0800

    File.kt: Add reading of file and output Bitmap or Drawable

--- lib/src/main/java/lib/github1552980358/ktExtension/android/java/File.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/java/File.kt
@@ -1,11 +1,13 @@
 package lib.github1552980358.ktExtension.android.java
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import lib.github1552980358.ktExtension.android.graphics.toBitmap
 import lib.github1552980358.ktExtension.android.graphics.writeJPEGToFile
 import lib.github1552980358.ktExtension.android.graphics.writePNGToFile
 import lib.github1552980358.ktExtension.android.graphics.writeWEBPToFile
+import lib.github1552980358.ktExtension.jvm.javaKeyword.tryCatch
 import java.io.File
 
 /**
@@ -19,10 +21,35 @@ fun File.writeBitmap(bitmap: Bitmap, compressFormat: Bitmap.CompressFormat = Bit
     }
 }
 
+/**
+ * Write [Drawable] into [File]
+ **/
 fun File.writeDrawable(drawable: Drawable, compressFormat: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG, quality: Int = 100) {
     when (compressFormat) {
         Bitmap.CompressFormat.PNG   -> drawable.toBitmap()?.writePNGToFile(this, quality)
         Bitmap.CompressFormat.JPEG  -> drawable.toBitmap()?.writeJPEGToFile(this, quality)
         Bitmap.CompressFormat.WEBP  -> drawable.toBitmap()?.writeWEBPToFile(this, quality)
     }
+}
+
+/**
+ * Read file and return as [Bitmap]
+ **/
+fun File.readAsBitmap(): Bitmap? {
+    var bitmap: Bitmap? = null
+    inputStream().use { `is` ->
+        tryCatch { bitmap = BitmapFactory.decodeStream(`is`) }
+    }
+    return bitmap
+}
+
+/**
+ * Read file and return as [Drawable]
+ **/
+fun File.readAsDrawable(srcName: String = "DRAWABLE"): Drawable? {
+   var drawable: Drawable? = null
+    inputStream().use { `is` ->
+        tryCatch { drawable = Drawable.createFromStream(`is`, srcName) }
+    }
+    return drawable
 }
