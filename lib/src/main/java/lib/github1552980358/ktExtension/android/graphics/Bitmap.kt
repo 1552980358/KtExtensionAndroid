ecd87afed0f217b40c8b1a32eb834d672f5df445
1552980358 <1552980358@qq.com>
2020-06-06 12:15:43 +0800

    Bitmap.kt: Add Bitmap.cutSquare

--- lib/src/main/java/lib/github1552980358/ktExtension/android/graphics/Bitmap.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/graphics/Bitmap.kt
@@ -5,6 +5,10 @@ package lib.github1552980358.ktExtension.android.graphics
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
 import android.graphics.drawable.BitmapDrawable
 import androidx.core.graphics.drawable.toDrawable
 import lib.github1552980358.ktExtension.jvm.javaKeyword.tryCatch
@@ -59,3 +63,42 @@ fun Bitmap.getByteArray(format: Bitmap.CompressFormat = Bitmap.CompressFormat.PN
     return byteArray
 }
 
+fun Bitmap.copy(): Bitmap? = Bitmap.createBitmap(this)
+
+/**
+ * Cut image as square
+ **/
+fun Bitmap.cutSquare(): Bitmap? {
+    if (width == 0 || height == 0) {
+        return null
+    }
+    
+    return when {
+        width == height -> {
+            Bitmap.createBitmap(this, 0, 0, width, width)
+        }
+        width > height -> {
+            Bitmap.createBitmap(this, (width - height) / 2, 0, width, width)
+        }
+        else -> {
+            Bitmap.createBitmap(this, 0, (height - width) / 2, width, width)
+        }
+    }
+    
+}
+
+fun Bitmap.cutSquare(centerX: Int, centerY: Int, sideLength: Int): Bitmap? {
+    if (centerX <= 0 || centerX >= width) {
+        return null
+    }
+    
+    if (centerY <= 0 || centerX >= height) {
+        return null
+    }
+    
+    if (width > height && sideLength > height) {
+        return null
+    }
+    
+    return Bitmap.createBitmap(this, (centerX - sideLength / 2), (centerY - sideLength/ 2) , sideLength, sideLength)
+}
