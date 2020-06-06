5061ab18576ef3d9919b0fa3cfab2a9664b56358
1552980358 <1552980358@qq.com>
2020-06-06 10:15:31 +0800

    Drawable.kt: Implement Drawable.toBitmap() method

--- lib/src/main/java/lib/github1552980358/ktExtension/android/graphics/Drawable.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/graphics/Drawable.kt
@@ -7,18 +7,16 @@ import android.graphics.drawable.Drawable
 import androidx.core.graphics.drawable.toBitmap
 
 /**
- * toBitmap will not be coded and implemented, because androidx.core
- * has provided official conversion method [Drawable.toBitmap].
+ * androidx.core has provided official conversion method [Drawable.toBitmap].
  * Add
  * implementation 'androidx.core:core-ktx:1.3.0'
  * into dependencies of build.gradle, so that [Drawable.toBitmap]
- * is available.
+ * is available for following converting method.
  **/
+fun Drawable.toBitmap(): Bitmap? = toBitmap(intrinsicWidth, intrinsicHeight)
 
 /** Convert into [ByteArray] **/
 fun Drawable.toByteArray(
     format: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG,
     quality: Int = 100
-): ByteArray? {
-    return toBitmap(intrinsicWidth, intrinsicHeight).getByteArray(format, quality)
-}
+): ByteArray? = toBitmap().getByteArray(format, quality)
