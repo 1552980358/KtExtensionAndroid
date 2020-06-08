ef08c9b03b18ad4e2e0fbcf43eff8a08f60d2013
1552980358 <1552980358@qq.com>
2020-06-08 22:25:58 +0800

    Update Log.kt

---
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/util/Log.kt
@@ -0,0 +1,26 @@
+package lib.github1552980358.ktExtension.android.util
+
+import android.util.Log
+
+
+/**
+ * Quick call of log output
+ **/
+
+fun logV(tag: String, msg: String) = Log.e(tag, msg)
+fun logV(tag: String, msg: String, throwable: Throwable) = Log.e(tag, msg, throwable)
+
+fun logD(tag: String, msg: String) = Log.d(tag, msg)
+fun logD(tag: String, msg: String, throwable: Throwable) = Log.d(tag, msg, throwable)
+
+fun logI(tag: String, msg: String) = Log.i(tag, msg)
+fun logI(tag: String, msg: String, throwable: Throwable) = Log.i(tag, msg, throwable)
+
+fun logW(tag: String, msg: String) = Log.w(tag, msg)
+fun logW(tag: String, msg: String, throwable: Throwable) = Log.w(tag, msg, throwable)
+
+fun logE(tag: String, msg: String) = Log.e(tag, msg)
+fun logE(tag: String, msg: String, throwable: Throwable) = Log.e(tag, msg, throwable)
+
+fun logWTF(tag: String, msg: String) = Log.wtf(tag, msg)
+fun logWTF(tag: String, msg: String, throwable: Throwable) = Log.wtf(tag, msg, throwable)
