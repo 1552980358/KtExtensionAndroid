db955a0aaf8eaaddfd01144645c7e8e5df1267a0
1552980358 <1552980358@qq.com>
2020-09-13 19:38:12 +0800

    View.kt: Add `getDimension`, `getDimensionPixelOffset`, `getDimensionPixelSize`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/view/View.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/view/View.kt
@@ -1,16 +1,27 @@
 package lib.github1552980358.ktExtension.android.view
 
+import android.content.Context
 import android.view.View
+import androidx.annotation.DimenRes
 import androidx.annotation.StringRes
 
 /**
- * Get [String] with [resId] without calling [Context].[getString]
+ * Get [String] with [resId] without calling [Context.getString]
  **/
 fun View.getString(@StringRes resId: Int) = context.getString(resId)
 
 fun View.getString(@StringRes resId: Int, vararg formatArgs: Any) = context.getString(resId, formatArgs)
 
 /**
- * Get [CharSequence] with [resId] without calling [Context].[getText]
+ * Get [CharSequence] with [resId] without calling [Context.getText]
  **/
 fun View.getText(@StringRes resId: Int) = context.getText(resId)
+
+/**
+ * Get Dimensions with [resId] without calling [Context.getResources]
+ **/
+fun View.getDimension(@DimenRes resId: Int) = context.resources.getDimension(resId)
+
+fun View.getDimensionPixelOffset(@DimenRes resId: Int) = context.resources.getDimensionPixelOffset(resId)
+
+fun View.getDimensionPixelSize(@DimenRes resId: Int) = context.resources.getDimensionPixelSize(resId)
