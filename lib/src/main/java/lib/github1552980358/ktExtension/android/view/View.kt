1bd1115c720129c98fea049dd4f2a184f5fd565f
1552980358 <1552980358@qq.com>
2020-09-13 19:43:03 +0800

    View.kt: Add getColor
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/view/View.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/view/View.kt
@@ -2,8 +2,24 @@ package lib.github1552980358.ktExtension.android.view
 
 import android.content.Context
 import android.view.View
+import androidx.annotation.ColorRes
 import androidx.annotation.DimenRes
 import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
+
+/**
+ * Get Color Int with [resId] without calling [ContextCompat.getColor]
+ **/
+fun View.getColor(@ColorRes resId: Int) = ContextCompat.getColor(context, resId)
+
+/**
+ * Get Dimensions with [resId] without calling [Context.getResources]
+ **/
+fun View.getDimension(@DimenRes resId: Int) = context.resources.getDimension(resId)
+
+fun View.getDimensionPixelOffset(@DimenRes resId: Int) = context.resources.getDimensionPixelOffset(resId)
+
+fun View.getDimensionPixelSize(@DimenRes resId: Int) = context.resources.getDimensionPixelSize(resId)
 
 /**
  * Get [String] with [resId] without calling [Context.getString]
@@ -16,12 +32,3 @@ fun View.getString(@StringRes resId: Int, vararg formatArgs: Any) = context.getS
  * Get [CharSequence] with [resId] without calling [Context.getText]
  **/
 fun View.getText(@StringRes resId: Int) = context.getText(resId)
-
-/**
- * Get Dimensions with [resId] without calling [Context.getResources]
- **/
-fun View.getDimension(@DimenRes resId: Int) = context.resources.getDimension(resId)
-
-fun View.getDimensionPixelOffset(@DimenRes resId: Int) = context.resources.getDimensionPixelOffset(resId)
-
-fun View.getDimensionPixelSize(@DimenRes resId: Int) = context.resources.getDimensionPixelSize(resId)
