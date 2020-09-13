2a39665d514f417acd078397746bb956edbd2240
1552980358 <1552980358@qq.com>
2020-09-13 19:35:23 +0800

    View.kt: Add `getText`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/view/View.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/view/View.kt
@@ -4,8 +4,13 @@ import android.view.View
 import androidx.annotation.StringRes
 
 /**
- * Get string with [resId] without calling [Context].[getString]
+ * Get [String] with [resId] without calling [Context].[getString]
  **/
 fun View.getString(@StringRes resId: Int) = context.getString(resId)
 
 fun View.getString(@StringRes resId: Int, vararg formatArgs: Any) = context.getString(resId, formatArgs)
+
+/**
+ * Get [CharSequence] with [resId] without calling [Context].[getText]
+ **/
+fun View.getText(@StringRes resId: Int) = context.getText(resId)
