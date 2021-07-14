557812526b410616a87d5c652bc028ec557718a4
1552980358 <1552980358@qq.com>
2021-07-14 18:18:10 +0800

    Fragment: Update findActivityViewById and requireTypedActivity
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/androidx/fragment/app/Fragment.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/androidx/fragment/app/Fragment.kt
@@ -1,18 +1,31 @@
 package lib.github1552980358.ktExtension.androidx.fragment.app
 
 import android.graphics.drawable.Drawable
+import android.view.View
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
+import androidx.annotation.IdRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 
 /**
  * Get Color with [resId]
  **/
-fun Fragment.getColor(@StringRes resId: Int) = ContextCompat.getColor(requireContext(), resId)
+fun Fragment.getColor(@ColorRes resId: Int) = ContextCompat.getColor(requireContext(), resId)
 
 /**
  * Get [Drawable] with [resId]
  **/
 fun Fragment.getDrawable(@DrawableRes resId: Int) = requireContext().getDrawable(resId)
 
+/**
+ * Find a [V] typed [View] with [resId] from parent [FragmentActivity]
+ **/
+fun <V: View> Fragment.findActivityViewById(@IdRes resId: Int): V? = requireActivity().findViewById(resId)
+
+/**
+ * Get [F] typed [FragmentActivity] with [Fragment.requireActivity]
+ **/
+@Suppress("UNCHECKED_CAST")
+fun <F: FragmentActivity> Fragment.requireTypedActivity() = requireActivity() as F
