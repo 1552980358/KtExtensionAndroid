5409747ceaeb0bb3298f4df6fb7d077cac4224fe
1552980358 <1552980358@qq.com>
2021-07-21 18:23:56 +0800

    Context.kt: Update checkPermission(), checkPermissionList(), checkPermissions(), checkPermissionArray()
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/content/Context.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/content/Context.kt
@@ -7,7 +7,9 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import lib.github1552980358.ktExtension.android.content.res.getStatusBarHeight
 
 /** Refer to [android.content.res.Resources.getStatusBarHeight] **/
@@ -78,3 +80,30 @@ fun Context.startActivity(activity: Class<Activity>, intent: Intent.(Intent) -> 
  * Directly start an activity by calling [Context.startActivity] with class [activity]
  **/
 fun Context.startActivity(activity: Class<Activity>) = startActivity(Intent(this, activity))
+
+/**
+ * Check [permission]
+ * @return [Boolean]
+ *      1. [true] if all permissions are gained;
+ *      2. [false] if some / all permissions are not gained
+ **/
+fun Context.checkPermission(permission: String) = ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+
+/**
+ * Check [permissions]
+ * @param permissions
+ * @return same as [Context.checkPermission]
+ **/
+fun Context.checkPermissionList(permissions: List<String>): Boolean {
+    for (permission in permissions) {
+        if (!checkPermission(permission)) {
+            return false
+        }
+    }
+    return true
+}
+
+fun Context.checkPermissions(vararg permissions: String) = checkPermissionList(permissions.toList())
+
+fun Context.checkPermissionArray(permissions: Array<String>) = checkPermissionList(permissions.toList())
+
