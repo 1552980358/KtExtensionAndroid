8f61a6458806db50e0c6a589132b9f19704aac24
1552980358 <1552980358@qq.com>
2021-08-26 13:44:51 +0800

    Bundle.kt: Fix incorrect return type on 'bundle'
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/os/Bundle.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/os/Bundle.kt
@@ -2,4 +2,4 @@ package lib.github1552980358.ktExtension.android.os
 
 import android.os.Bundle
 
-fun bundle(block: Bundle.(Bundle) -> Bundle) = Bundle().apply { block(this) }
+fun bundle(block: Bundle.(Bundle) -> Unit) = Bundle().apply { block(this) }
