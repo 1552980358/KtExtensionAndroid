ad71aa9b3dc9d11545bd927fc2257138eea72deb
1552980358 <1552980358@qq.com>
2021-11-24 16:30:17 +0800

    Intent.kt: Update getSerializableOf()
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/content/Intent.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/content/Intent.kt
@@ -3,6 +3,7 @@ package lib.github1552980358.ktExtension.android.content
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import java.io.Serializable
 
 /**
  * Create a new [Intent] instance with [block]
@@ -19,3 +20,6 @@ fun intent(action: String, uri: Uri, block: Intent.(Intent) -> Unit) = Intent(ac
 
 fun intent(action: String, uri: Uri, context: Context, clazz: Class<out Any>, block: Intent.(Intent) -> Unit) =
     Intent(action, uri, context, clazz).apply { block(this) }
+
+@Suppress("UNCHECKED_CAST")
+fun <T: Serializable> Intent.getSerializableOf(name: String) = getSerializableExtra(name) as T?
