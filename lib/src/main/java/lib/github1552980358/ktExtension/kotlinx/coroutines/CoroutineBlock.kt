9adc95867d1f94781b9c7d9ead15a527f2bbcfbc
1552980358 <1552980358@qq.com>
2022-01-12 01:01:17 +0800

    CoroutineBlock.kt.kt: Create CoroutineBlock.kt and methods

---
+++ lib/src/main/java/lib/github1552980358/ktExtension/kotlinx/coroutines/CoroutineBlock.kt
@@ -0,0 +1,104 @@
+package lib.github1552980358.ktExtension.kotlinx.coroutines
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+abstract class CoroutineBlock(protected val coroutineScope: CoroutineScope, protected val block: suspend CoroutineScope.() -> Unit) {
+    /**
+     * Method to be called for running
+     **/
+    abstract suspend fun runBlock()
+}
+
+class CoroutineTry(coroutineScope: CoroutineScope, block: suspend CoroutineScope.() -> Unit)
+    : CoroutineBlock(coroutineScope, block) {
+    
+    private var catchBlock: CoroutineCatch? = null
+    private var finallyBlock: CoroutineFinally? = null
+    
+    /**
+     * Set [catchBlock] to [ioDispatcher]
+     **/
+    fun ioCatch(catchBlock: suspend CoroutineScope.(Exception) -> Unit) =
+        setCatch(ioDispatcher, catchBlock)
+    
+    /**
+     * Set [catchBlock] to [mainDispatcher]
+     **/
+    fun mainCatch(catchBlock: suspend CoroutineScope.(Exception) -> Unit) =
+        setCatch(mainDispatcher, catchBlock)
+    
+    /**
+     * Set [catchBlock] to [coroutineScope]
+     **/
+    fun thenCatch(catchBlock: suspend CoroutineScope.(Exception) -> Unit) =
+        setCatch(coroutineScope, catchBlock)
+    
+    /**
+     * Set [catchBlock] to specified [CoroutineScope]
+     **/
+    fun setCatch(coroutineScope: CoroutineScope, catchBlock: suspend CoroutineScope.(Exception) -> Unit) = apply {
+        this.catchBlock = CoroutineCatch(coroutineScope, catchBlock)
+    }
+    
+    /**
+     * Set [finallyBlock] to [ioDispatcher]
+     **/
+    fun ioFinally(finallyBlock: suspend CoroutineScope.() -> Unit) =
+        setFinally(ioDispatcher, finallyBlock)
+    
+    /**
+     * Set [finallyBlock] to [mainDispatcher]
+     **/
+    fun mainFinally(finallyBlock: suspend CoroutineScope.() -> Unit) =
+        setFinally(mainDispatcher, finallyBlock)
+    
+    /**
+     * Set [finallyBlock] to [coroutineScope]
+     **/
+    fun thenFinally(finallyBlock: suspend CoroutineScope.() -> Unit) =
+        setFinally(coroutineScope, finallyBlock)
+    
+    /**
+     * Set [finallyBlock] to specified [CoroutineScope]
+     **/
+    fun setFinally(coroutineScope: CoroutineScope, finallyBlock: suspend CoroutineScope.() -> Unit) = apply {
+        this.finallyBlock = CoroutineFinally(coroutineScope, finallyBlock)
+    }
+    
+    /**
+     * Run [runBlock] at [coroutineScope]
+     */
+    fun launch() = coroutineScope.launch { runBlock() }
+    
+    /**
+     * Run block, with specified content
+     */
+    override suspend fun runBlock() {
+        try {
+            block(coroutineScope)
+        } catch (e: Exception) {
+            catchBlock?.onCatch(e)
+        } finally {
+            finallyBlock?.runBlock()
+        }
+    }
+    
+}
+
+class CoroutineCatch(private val coroutineScope: CoroutineScope, private val block: suspend CoroutineScope.(Exception) -> Unit) {
+    /**
+     * Called at catch { ... }
+     **/
+    suspend fun onCatch(exception: Exception) = coroutineScope.launch { block(exception) }
+}
+
+class CoroutineFinally(coroutineScope: CoroutineScope, block: suspend CoroutineScope.() -> Unit)
+    : CoroutineBlock(coroutineScope, block) {
+    /**
+     * Called at finally { ... }
+     **/
+    override suspend fun runBlock() {
+        coroutineScope.launch { block() }
+    }
+}
