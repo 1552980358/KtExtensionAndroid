0aa78d4984588fa7ba4ab8c277655debec8bad5d
1552980358 <1552980358@qq.com>
2022-01-14 19:29:40 +0800

    Methods move to KtExtension
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/kotlin/Array.kt
+++
@@ -1,3 +0,0 @@
-package lib.github1552980358.ktExtension.kotlin
-
-fun <charSequence: CharSequence> Array<out charSequence>.joinTogether() = joinToString(separator = "")
--- lib/src/main/java/lib/github1552980358/ktExtension/kotlin/collections/List.kt
+++
@@ -1,3 +0,0 @@
-package lib.github1552980358.ktExtension.kotlin.collections
-
-fun <charSequence: CharSequence> List<charSequence>.joinTogether() = joinToString(separator = "")
