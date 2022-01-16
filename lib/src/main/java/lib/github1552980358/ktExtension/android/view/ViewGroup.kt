2373056c364c9fd4b4bf448e2c4bd3b7a6d0c156
1552980358 <1552980358@qq.com>
2022-01-16 11:58:33 +0800

    ViewGroup.kt: Update method createViewHolder()
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/view/ViewGroup.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/view/ViewGroup.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 @Suppress("UNCHECKED_CAST")
 fun <viewType: View> ViewGroup.inflateAs(@LayoutRes resId: Int, attachToRoot: Boolean = false): viewType =
@@ -11,3 +12,6 @@ fun <viewType: View> ViewGroup.inflateAs(@LayoutRes resId: Int, attachToRoot: Bo
  
 fun ViewGroup.inflate(@LayoutRes resId: Int, attachToRoot: Boolean = false) =
     LayoutInflater.from(context).inflate(resId, this, attachToRoot)
+
+inline fun <reified viewHolder: ViewHolder> ViewGroup.createViewHolder(@LayoutRes resId: Int, attachToRoot: Boolean = false): viewHolder =
+    viewHolder::class.java.getConstructor(View::class.java).apply { isAccessible = true }.newInstance(inflate(resId, attachToRoot))
