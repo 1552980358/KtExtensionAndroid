b8827253ab6f470831c4eb4c25e8f8d9e4cef470
1552980358 <1552980358@qq.com>
2022-01-30 21:45:51 +0800

    ViewGroup.kt: Replace old method
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/view/ViewGroup.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/view/ViewGroup.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import lib.github1552980358.ktExtension.jvm.keyword.tryRun
 
 @Suppress("UNCHECKED_CAST")
 fun <viewType: View> ViewGroup.inflateAs(@LayoutRes resId: Int, attachToRoot: Boolean = false): viewType =
@@ -14,4 +15,4 @@ fun ViewGroup.inflate(@LayoutRes resId: Int, attachToRoot: Boolean = false) =
     LayoutInflater.from(context).inflate(resId, this, attachToRoot)
 
 inline fun <reified viewHolder: ViewHolder> ViewGroup.createViewHolder(@LayoutRes resId: Int, attachToRoot: Boolean = false): viewHolder =
-    viewHolder::class.java.getConstructor(View::class.java).apply { isAccessible = true }.newInstance(inflate(resId, attachToRoot))
+    tryRun { viewHolder::class.constructors.find { it.parameters.size == 1 }?.call(inflate(resId, attachToRoot)) } ?: throw NoSuchMethodException()
