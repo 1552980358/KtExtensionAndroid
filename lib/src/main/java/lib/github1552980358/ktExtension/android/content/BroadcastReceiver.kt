6eb3f28ee5fba7936f6da8fbe8ead46b5bfac1ca
1552980358 <1552980358@qq.com>
2022-03-19 19:28:49 +0800

    BroadcastReceiver.kt: Update methods
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/content/BroadcastReceiver.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/content/BroadcastReceiver.kt
@@ -7,37 +7,44 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 
-/***********************************************************
- * fun example(context: Context) {                         *
- *     broadcastReceiver { context, intent, receiver ->    *
- *         intent?:return@broadcastReceiver                *
- *         when (intent.action) {                          *
- *             "ACTION1" -> { /** ... **/ }                *
- *             "ACTION2" -> { /** ... **/ }                *
- *         }                                               *
- *         context?.unregisterReceiver(receiver)           *
- *     }.register(context, arrayOf("ACTION1", "ACTION2"))  *
- * }                                                       *
- ***********************************************************/
-
-/**
- * Creating a [BroadcastReceiver] object with overriding [BroadcastReceiver.onReceive] only
- **/
-fun broadcastReceiver(block: (context: Context?, intent: Intent?, receiver: BroadcastReceiver) -> Unit) =
-    object: BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            block(context, intent, this)
+object BroadcastReceiver {
+    
+    /***********************************************************
+     * fun example(context: Context) {                         *
+     *     broadcastReceiver { context, intent, receiver ->    *
+     *         intent?:return@broadcastReceiver                *
+     *         when (intent.action) {                          *
+     *             "ACTION1" -> { /** ... **/ }                *
+     *             "ACTION2" -> { /** ... **/ }                *
+     *         }                                               *
+     *         context?.unregisterReceiver(receiver)           *
+     *     }.register(context, arrayOf("ACTION1", "ACTION2"))  *
+     * }                                                       *
+     ***********************************************************/
+    
+    /**
+     * Creating a [BroadcastReceiver] object with overriding [BroadcastReceiver.onReceive] only
+     **/
+    fun broadcastReceiver(block: (context: Context?, intent: Intent?, receiver: BroadcastReceiver) -> Unit) =
+        object: BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                block(context, intent, this)
+            }
         }
-    }
-
-/**
- * Register [BroadcastReceiver]
- **/
-fun BroadcastReceiver.register(context: Context, vararg actions: String) =
-    context.registerBroadcastReceiver(this, actions)
-
-fun BroadcastReceiver.register(context: Context, intentFilter: IntentFilter.() -> Unit) =
-    context.registerBroadcastReceiver(this, intentFilter)
-
-fun BroadcastReceiver.register(context: Context, action: String) =
-    context.registerBroadcastReceiver(this, action)
+    
+    /**
+     * Register [BroadcastReceiver]
+     **/
+    @JvmStatic
+    fun BroadcastReceiver.register(context: Context, vararg actions: String) =
+        context.registerBroadcastReceiver(this, actions)
+    
+    @JvmStatic
+    fun BroadcastReceiver.register(context: Context, intentFilter: IntentFilter.() -> Unit) =
+        context.registerBroadcastReceiver(this, intentFilter)
+    
+    @JvmStatic
+    fun BroadcastReceiver.register(context: Context, action: String) =
+        context.registerBroadcastReceiver(this, action)
+    
+}
