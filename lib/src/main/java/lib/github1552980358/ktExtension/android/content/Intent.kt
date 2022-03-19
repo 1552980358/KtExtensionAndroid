9ff04b42c28c34618451ea3075f9c248c2bfaa2e
1552980358 <1552980358@qq.com>
2022-03-19 19:30:14 +0800

    Intent.kt: Update methods
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/content/Intent.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/content/Intent.kt
@@ -5,21 +5,32 @@ import android.content.Intent
 import android.net.Uri
 import java.io.Serializable
 
-/**
- * Create a new [Intent] instance with [block]
- **/
-fun intent(block: Intent.(Intent) -> Unit) = Intent().apply{ block(this) }
-
-fun intent(o: Intent, block: Intent.(Intent) -> Unit) = Intent(o).apply { block(this) }
-
-fun intent(context: Context, clazz: Class<out Any>, block: Intent.(Intent) -> Unit) = Intent(context, clazz).apply { block(this) }
-
-fun intent(action: String, block: Intent.(Intent) -> Unit) = Intent(action).apply{ block(this) }
-
-fun intent(action: String, uri: Uri, block: Intent.(Intent) -> Unit) = Intent(action, uri).apply { block(this) }
-
-fun intent(action: String, uri: Uri, context: Context, clazz: Class<out Any>, block: Intent.(Intent) -> Unit) =
-    Intent(action, uri, context, clazz).apply { block(this) }
-
-@Suppress("UNCHECKED_CAST")
-fun <T: Serializable> Intent.getSerializableOf(name: String) = getSerializableExtra(name) as T?
+object Intent {
+    
+    /**
+     * Create a new [Intent] instance with [block]
+     **/
+    @JvmStatic
+    fun intent(block: Intent.(Intent) -> Unit) = Intent().apply{ block(this) }
+    
+    @JvmStatic
+    fun intent(o: Intent, block: Intent.(Intent) -> Unit) = Intent(o).apply { block(this) }
+    
+    @JvmStatic
+    fun intent(context: Context, clazz: Class<out Any>, block: Intent.(Intent) -> Unit) = Intent(context, clazz).apply { block(this) }
+    
+    @JvmStatic
+    fun intent(action: String, block: Intent.(Intent) -> Unit) = Intent(action).apply{ block(this) }
+    
+    @JvmStatic
+    fun intent(action: String, uri: Uri, block: Intent.(Intent) -> Unit) = Intent(action, uri).apply { block(this) }
+    
+    @JvmStatic
+    fun intent(action: String, uri: Uri, context: Context, clazz: Class<out Any>, block: Intent.(Intent) -> Unit) =
+        Intent(action, uri, context, clazz).apply { block(this) }
+    
+    @Suppress("UNCHECKED_CAST")
+    @JvmStatic
+    fun <T: Serializable> Intent.getSerializableOf(name: String) = getSerializableExtra(name) as T?
+    
+}
