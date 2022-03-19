14271cf17be9f067b809fdaf5daade3972a31148
1552980358 <1552980358@qq.com>
2022-03-19 19:27:37 +0800

    File.kt: Update methods
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- lib/src/main/java/lib/github1552980358/ktExtension/android/java/File.kt
+++ lib/src/main/java/lib/github1552980358/ktExtension/android/java/File.kt
@@ -9,61 +9,43 @@ import lib.github1552980358.ktExtension.android.graphics.toBitmap
 import lib.github1552980358.ktExtension.android.graphics.writeJPEGToFile
 import lib.github1552980358.ktExtension.android.graphics.writePNGToFile
 import lib.github1552980358.ktExtension.android.graphics.writeWEBPToFile
-import lib.github1552980358.ktExtension.jvm.keyword.tryCatch
+import lib.github1552980358.ktExtension.jvm.keyword.tryRun
 import java.io.File
 
-/**
- * Write [Bitmap] into [File]
- **/
-fun File.writeBitmap(bitmap: Bitmap, compressFormat: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG, quality: Int = 100) {
-    when (compressFormat) {
-        Bitmap.CompressFormat.PNG   -> bitmap.writePNGToFile(this, quality)
-        Bitmap.CompressFormat.JPEG  -> bitmap.writeJPEGToFile(this, quality)
-        Bitmap.CompressFormat.WEBP  -> bitmap.writeWEBPToFile(this, quality)
+object File {
+    
+    /**
+     * Write [Bitmap] into [File]
+     **/
+    fun File.writeBitmap(bitmap: Bitmap, compressFormat: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG, quality: Int = 100) {
+        @Suppress("DEPRECATION")
+        when (compressFormat) {
+            Bitmap.CompressFormat.PNG   -> bitmap.writePNGToFile(this, quality)
+            Bitmap.CompressFormat.JPEG  -> bitmap.writeJPEGToFile(this, quality)
+            Bitmap.CompressFormat.WEBP  -> bitmap.writeWEBPToFile(this, quality)
+            else -> bitmap.writeWEBPToFile(this, quality)
+        }
     }
-}
-
-/**
- * Write [Drawable] into [File]
- **/
-fun File.writeDrawable(drawable: Drawable, compressFormat: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG, quality: Int = 100) {
-    when (compressFormat) {
-        Bitmap.CompressFormat.PNG   -> drawable.toBitmap()?.writePNGToFile(this, quality)
-        Bitmap.CompressFormat.JPEG  -> drawable.toBitmap()?.writeJPEGToFile(this, quality)
-        Bitmap.CompressFormat.WEBP  -> drawable.toBitmap()?.writeWEBPToFile(this, quality)
+    
+    /**
+     * Write [Drawable] into [File]
+     **/
+    fun File.writeDrawable(drawable: Drawable, compressFormat: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG, quality: Int = 100) {
+        @Suppress("DEPRECATION")
+        when (compressFormat) {
+            Bitmap.CompressFormat.PNG   -> drawable.toBitmap()?.writePNGToFile(this, quality)
+            Bitmap.CompressFormat.JPEG  -> drawable.toBitmap()?.writeJPEGToFile(this, quality)
+            Bitmap.CompressFormat.WEBP  -> drawable.toBitmap()?.writeWEBPToFile(this, quality)
+            else -> drawable.toBitmap()?.writeWEBPToFile(this, quality)
+        }
     }
-}
-
-/**
- * Read file and return as [Bitmap]
- *
- * null will be returned if exception is thrown during conversion or
- * empty file
- **/
-fun File.readAsBitmap(): Bitmap? {
-    var bitmap: Bitmap? = null
-    if (length() == 0L) {
-        return null
+    
+    @JvmStatic
+    val File.asBitmap: Bitmap? get() {
+        if (length() == 0L) {
+            return null
+        }
+        return tryRun { inputStream().use { `is` -> BitmapFactory.decodeStream(`is`) } }
     }
-    inputStream().use { `is` ->
-        tryCatch { bitmap = BitmapFactory.decodeStream(`is`) }
-    }
-    return bitmap
-}
-
-/**
- * Read file and return as [Drawable]
- *
- * null will be returned if exception is thrown during conversion or
- * empty file
- **/
-fun File.readAsDrawable(srcName: String = "DRAWABLE"): Drawable? {
-   var drawable: Drawable? = null
-    if (length() == 0L) {
-        return null
-    }
-    inputStream().use { `is` ->
-        tryCatch { drawable = Drawable.createFromStream(`is`, srcName) }
-    }
-    return drawable
+    
 }
